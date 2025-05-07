787b2dea51c307e3e7134af5d3a3b8179fd37d6b
yiyi <yiyiw2002@gmail.com>
2025-05-07 22:56:53 +0200

    Modificación corazones

--- src/pages/CharacterSelection/CharacterSelection.tsx
+++ src/pages/CharacterSelection/CharacterSelection.tsx
@@ -12,28 +12,58 @@ const CharacterSelection: React.FC = () => {
           <div className="cat_1">Cat 1</div>
           <div className="cat_2">Cat 2</div>
           <div className="cat_3">Cat 3</div>
-          <div className="cat_4">Cat 4</div>
         </div>
 
         <div className="container_cats_status">
           <div className="status_cat_1 status-cat">
             <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
             <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
             <img src={img2} alt="Happiness" />
           </div>
           <div className="status_cat_2 status-cat">
             <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
             <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
             <img src={img2} alt="Happiness" />
           </div>
           <div className="status_cat_3 status-cat">
             <img src={img1} alt="Food" />
-            <img src={img3} alt="Water" />
-            <img src={img2} alt="Happiness" />
-          </div>
-          <div className="status_cat_4 status-cat">
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
+            <img src={img1} alt="Food" />
             <img src={img1} alt="Food" />
             <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img3} alt="Water" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
+            <img src={img2} alt="Happiness" />
             <img src={img2} alt="Happiness" />
           </div>
         </div>
