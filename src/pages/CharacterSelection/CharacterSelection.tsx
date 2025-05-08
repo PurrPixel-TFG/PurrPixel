52ea6b7ee922a4b512ac7c1d5d2fdc221dca841d
Fabs <Fabiolaayonperez97@gmail.com>
2025-05-08 13:32:15 +0200

    08.05.2025

--- src/pages/CharacterSelection/CharacterSelection.tsx
+++ src/pages/CharacterSelection/CharacterSelection.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './CharacterSelection.scss';
 import img1 from '../../assets/images/statusHeart_Food.webp';
 import img2 from '../../assets/images/statusHeart_Happiness.png';
 import img3 from '../../assets/images/statusHeart_water.png';
 
 const CharacterSelection: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const handleCatClick = () => {
+    navigate('/home-page');
+  };
+  
   return (
     <>
       <main className="container_HomePage">
         <div className="container_cats">
-          <div className="cat_1">Cat 1</div>
-          <div className="cat_2">Cat 2</div>
-          <div className="cat_3">Cat 3</div>
+          <div className="cat_1" onClick={handleCatClick}>Cat 1</div>
+          <div className="cat_2" onClick={handleCatClick}>Cat 2</div>
+          <div className="cat_3" onClick={handleCatClick}>Cat 3</div>
         </div>
 
         <div className="container_cats_status">
