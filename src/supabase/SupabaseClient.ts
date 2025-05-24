ed36bfaf41f52dc406ee0f6e3cdfc2437112b343
Tнe Sleeρy Lαdy 🌸~ <fabiolaayonperez97@gmail.com>
2025-05-24 16:21:17 +0200

    Información de Supabase actualiza en src/supabase. Se han creado nuevos vínculos. Add to cart pasa a ser Buy

--- src/supabase/SupabaseClient.ts
+++ src/supabase/SupabaseClient.ts
@@ -1,7 +1,8 @@
 // supabase/SupabaseClient.ts
+// Password: WangYonMena
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nkbafyaohkooaazapcui.supabase.co' 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rYmFmeWFvaGtvb2FhemFwY3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTE1NTUsImV4cCI6MjA2MzE2NzU1NX0.iYKDzZtlBNVMMPfTERbmXjt0TwGSk_YocqTJtHx7UMw' 
+const supabaseUrl = 'https://nkbafyaohkooaazapcui.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rYmFmeWFvaGtvb2FhemFwY3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTE1NTUsImV4cCI6MjA2MzE2NzU1NX0.iYKDzZtlBNVMMPfTERbmXjt0TwGSk_YocqTJtHx7UMw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
